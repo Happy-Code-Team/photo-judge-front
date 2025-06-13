b227ff619dd00978d975c672ea27cd3d364bf5ab
张国伟 <1204378021@qq.com>
2025-06-13 16:47:45 +0800

    提交 axios 文件

---
+++ src/utils/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8089/photo', // 根据你的后端地址修改
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// 你可以在这里添加请求/响应拦截器
+// instance.interceptors.request.use(config => { ... });
+// instance.interceptors.response.use(response => { ... });
+
+export default instance;
+
