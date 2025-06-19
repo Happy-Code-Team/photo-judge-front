d0b5915474c5d598dd5ff4aa6b6d1c6bef52f7aa
seaflower <1204378021@qq.com>
2025-06-19 15:55:43 +0800

    增加前端流水线

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8089/photo', // 根据你的后端地址修改
+  // baseURL: 'http://localhost:8089/photo', // 根据你的后端地址修改
+  baseURL: '/photo',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
